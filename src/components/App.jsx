cbfb44694be631b1fa99a7636e180d91487bdf77
isthifa <4pa19cs404@pace.edu.in>
2023-07-21 17:18:47 +0530

    use ref

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
-import React,{createContext} from "react";
-import MainComponent from "./MainComponent";
+import React,{useEffect,useState,useRef} from "react";
 
-export const loginContext = createContext();
 export default function App() {
+  const [name,setName] = useState("");
+  const count=useRef(0);
 
+  const changeName=(e)=>{
+    setName(e.target.value);
+  }
+
+  useEffect(()=>{
+    count.current=count.current+1;
+  });
   return (
-    <loginContext.Provider value={true}>
     <div>
-      <MainComponent/>
+      <h1>App</h1>
+      <input type="text" onChange={changeName}/>
+      <h2>Name :{name}</h2>
+      <h2>Renders :{count.current}</h2>
     </div>
-    </loginContext.Provider>
   )
-
 }
