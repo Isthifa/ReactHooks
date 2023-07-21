1808eb380b71157ad8e50ade3973ff378035eb25
isthifa <4pa19cs404@pace.edu.in>
2023-07-21 16:18:39 +0530

    use Effects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,15 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 
 export default function App() {
-   const [details,setDetails]=useState({name:"",counter:0})
+  const [count, setCount] = useState(0);
 
-
-   const handleChange=(e)=>{
-      setDetails((prev)=>({
-        ...prev,
-        name:e.target.value,
-      }));
-    }
-    const decrement=()=>{
-      setDetails((prev)=>({
-        ...prev,
-        counter:prev.counter-1,
-      }));
-    }
-
-   const increment=()=>{
-      setDetails((prev)=>({
-        ...prev,
-        counter:prev.counter+1,
-      }));
-    }
+  useEffect(()=>{
+    document.title = `You clicked ${count} times`;
+  })
   return (
     <div>
-      <h1>React Hooks</h1>
-      <input type="text" onChange={handleChange}></input>
-      <h1>{details.name} Counter:{details.counter}</h1>
-        <button onClick={increment}>Increment</button> {/*<!-setCount is a function which is used to update the value of count>*/}
-        <button onClick={decrement}>Decrement</button>
+      <h1>{count} New Messages!</h1>
+      <button onClick={() => setCount(count + 1)}>count</button>
     </div>
   );
 }
