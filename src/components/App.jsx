ef9aa8d2062cd9463cd7cfef55cdc89c48ea63d0
isthifa <4pa19cs404@pace.edu.in>
2023-07-21 13:01:20 +0530

    another method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,14 @@
 import React,{useState} from "react";
 
 export default function App() {
-    const array=useState(0);
-    const count=array[0];
-    const setCount=array[1];
-    console.log(array);
-    const increaseCounter=()=>{
-        setCount(count+1);
-    }
-    const decreaseCounter=()=>{
-        setCount(count-1);
-    }
+    const [count,setCount]=useState(0);
 
   return (
     <div>
       <h1>React Hooks</h1>
       <h1>Counter:{count}</h1>
-    <button onClick={increaseCounter}>Increase</button>
-    <button onClick={decreaseCounter}>Decrease</button>
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <button onClick={()=>setCount(count-1)}>Decrement</button>
     </div>
   );
 }
