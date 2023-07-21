ab0f917f3de01fadb13398e6eee1637194e66c8b
isthifa <4pa19cs404@pace.edu.in>
2023-07-21 16:08:08 +0530

    new method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,35 @@
 import React,{useState} from "react";
 
 export default function App() {
-    const [count,setCount]=useState(0);
+   const [details,setDetails]=useState({name:"",counter:0})
 
+
+   const handleChange=(e)=>{
+      setDetails((prev)=>({
+        ...prev,
+        name:e.target.value,
+      }));
+    }
+    const decrement=()=>{
+      setDetails((prev)=>({
+        ...prev,
+        counter:prev.counter-1,
+      }));
+    }
+
+   const increment=()=>{
+      setDetails((prev)=>({
+        ...prev,
+        counter:prev.counter+1,
+      }));
+    }
   return (
     <div>
       <h1>React Hooks</h1>
-      <h1>Counter:{count}</h1>
-        <button onClick={()=>setCount(count+1)}>Increment</button>
-        <button onClick={()=>setCount(count-1)}>Decrement</button>
+      <input type="text" onChange={handleChange}></input>
+      <h1>{details.name} Counter:{details.counter}</h1>
+        <button onClick={increment}>Increment</button> {/*<!-setCount is a function which is used to update the value of count>*/}
+        <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
